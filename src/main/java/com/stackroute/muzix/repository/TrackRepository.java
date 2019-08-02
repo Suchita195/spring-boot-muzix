c75a7d4c5b177c87ca0d122a30e045e155532691
Suchita Kanchhal <suchitakanchhal1996@gmail.com>
2019-08-02 15:01:15 +0530

    submitted last fm assignment using mysql

---
+++ src/main/java/com/stackroute/muzix/repository/TrackRepository.java
@@ -0,0 +1,13 @@
+package com.stackroute.muzix.repository;
+
+import com.stackroute.muzix.domain.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrackRepository extends JpaRepository<Track,Integer> {
+  //Query to search using name
+  @Query("FROM Track u WHERE u.name = ?1")
+  Track findTrackByName(String name);
+}
