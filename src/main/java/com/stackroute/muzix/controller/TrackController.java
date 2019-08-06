209503bf4ba729397d466970d51fb4c38074f3f4
Suchita195 <51943061+Suchita195@users.noreply.github.com>
2019-08-06 17:52:12 +0530

    used encapsulation and removed unwanted code

--- src/main/java/com/stackroute/muzix/controller/TrackController.java
+++ src/main/java/com/stackroute/muzix/controller/TrackController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value="api/v1")
 public class TrackController extends GlobalExceptionHandler {
-  TrackService trackService;
+  private TrackService trackService;
 
   @Autowired
   public TrackController(TrackService trackService) {
@@ -26,12 +26,9 @@ public class TrackController extends GlobalExceptionHandler {
   @PostMapping("track")
   public ResponseEntity<?> saveTrack (@RequestBody Track track) throws TrackAlreadyExistsException{
     ResponseEntity responseEntity;
-    //try
+   
         trackService.saveTrack(track);
         responseEntity = new ResponseEntity<String>("successfully created", HttpStatus.CREATED);
-
-      //catch (TrackAlreadyExistsException e) {
-      //responseEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
 
     return responseEntity;
   }
@@ -40,12 +37,11 @@ public class TrackController extends GlobalExceptionHandler {
   @PutMapping("track")
   public ResponseEntity<?> updateTrack(@RequestBody Track track) {
     ResponseEntity responseEntity;
-    //try {
+    
         trackService.updateTrack(track);
         responseEntity = new ResponseEntity<String>("successfully updated", HttpStatus.OK);
 
-    //catch (Exception e) {
-      //responseEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
+    
     return responseEntity;
   }
 
@@ -70,11 +66,9 @@ public class TrackController extends GlobalExceptionHandler {
   public ResponseEntity getTrackById (@PathVariable int id) throws TrackNotFoundException
   {
     ResponseEntity responseEntity;
-    //try {
+    
       responseEntity = new ResponseEntity<Track>(trackService.getTrackById(id),HttpStatus.OK);
-    //catch (TrackNotFoundException e) {
-      //responseEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
-    //}
+   
     return responseEntity;
   }
 
