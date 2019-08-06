93bf23ce0c291ef011a11aba72b7be3f6d060bae
Suchita195 <51943061+Suchita195@users.noreply.github.com>
2019-08-06 17:50:49 +0530

    used encapsulation

--- src/main/java/com/stackroute/muzix/controller/TrackController.java
+++ src/main/java/com/stackroute/muzix/controller/TrackController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value="api/v1")
 public class TrackController {
-  TrackService trackService;
+  private TrackService trackService;
 
   @Autowired
   public TrackController(TrackService trackService) {
