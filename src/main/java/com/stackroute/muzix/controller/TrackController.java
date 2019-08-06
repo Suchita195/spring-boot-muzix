dccaf71c6d0c4cf4c3b38ce704dc999f8367ad1b
Suchita195 <51943061+Suchita195@users.noreply.github.com>
2019-08-06 17:50:29 +0530

    used encapsulation and removed unwanted code

--- src/main/java/com/stackroute/muzix/controller/TrackController.java
+++ src/main/java/com/stackroute/muzix/controller/TrackController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value="api/v1")
 public class TrackController extends GlobalExceptionHandler {
-  TrackService trackService;
+  private TrackService trackService;
 
   @Autowired
   public TrackController(TrackService trackService) {
@@ -26,13 +26,8 @@ public class TrackController extends GlobalExceptionHandler {
   @PostMapping("track")
   public ResponseEntity<?> saveTrack (@RequestBody Track track) throws TrackAlreadyExistsException{
     ResponseEntity responseEntity;
-    //try
         trackService.saveTrack(track);
         responseEntity = new ResponseEntity<String>("successfully created", HttpStatus.CREATED);
-
-      //catch (TrackAlreadyExistsException e) {
-      //responseEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
-
     return responseEntity;
   }
 
@@ -40,12 +35,9 @@ public class TrackController extends GlobalExceptionHandler {
   @PutMapping("track")
   public ResponseEntity<?> updateTrack(@RequestBody Track track) {
     ResponseEntity responseEntity;
-    //try {
+  
         trackService.updateTrack(track);
         responseEntity = new ResponseEntity<String>("successfully updated", HttpStatus.OK);
-
-    //catch (Exception e) {
-      //responseEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
     return responseEntity;
   }
 
@@ -70,11 +62,7 @@ public class TrackController extends GlobalExceptionHandler {
   public ResponseEntity getTrackById (@PathVariable int id) throws TrackNotFoundException
   {
     ResponseEntity responseEntity;
-    //try {
       responseEntity = new ResponseEntity<Track>(trackService.getTrackById(id),HttpStatus.OK);
-    //catch (TrackNotFoundException e) {
-      //responseEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
-    //}
     return responseEntity;
   }
 
